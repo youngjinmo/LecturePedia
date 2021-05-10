4cebe03843890bdd825168af80b12ba77a9c483b
Youngjin Mo <dev.youngjinmo@gmail.com>
2021-05-10 17:26:13 +0900

    Initialization with bootstrap

---
+++ src/main/java/com/devandy/studypedia/StudyPediaApplication.java
@@ -0,0 +1,13 @@
+package com.devandy.studypedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyPediaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devandy/studypedia/StudyPediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devandy.studypedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
