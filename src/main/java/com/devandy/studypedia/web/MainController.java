3ac25bf4f31846ef97792743f35a45fda9367169
Youngjin Mo <dev.youngjinmo@gmail.com>
2021-05-10 19:53:01 +0900

    draft. 메인 페이지 컨텐츠 초안 적용

--- src/main/java/com/devandy/studypedia/web/MainController.java
+++ src/main/java/com/devandy/studypedia/web/MainController.java
@@ -9,4 +9,9 @@ public class MainController {
     public String home() {
         return "index";
     }
+
+    @GetMapping("/about")
+    public String about() {
+        return "/layout/about";
+    }
 }
