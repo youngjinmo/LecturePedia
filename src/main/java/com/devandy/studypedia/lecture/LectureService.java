95558a413608c3e1b0612dba6ff97cc8b6b24622
Youngjin Mo <dev.youngjinmo@gmail.com>
2021-05-25 20:44:52 +0900

    refactor. minor

--- src/main/java/com/devandy/studypedia/lecture/LectureService.java
+++ src/main/java/com/devandy/studypedia/lecture/LectureService.java
@@ -2,11 +2,9 @@ package com.devandy.studypedia.lecture;
 
 import com.devandy.studypedia.web.dto.lecture.RequestSaveLectureDto;
 import com.devandy.studypedia.web.dto.lecture.RequestUpdateLectureDto;
-import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.List;
 
-@Qualifier("lectureServiceImpl")
 public interface LectureService {
     void addLecture(RequestSaveLectureDto requestSaveLectureDto, Long authorId);
     Lecture getLecture(Long id);
