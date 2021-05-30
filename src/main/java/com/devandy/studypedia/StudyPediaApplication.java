cd247017867ac630e8c94d994218ef7546f17f80
Youngjin Mo <dev.youngjinmo@gmail.com>
2021-05-30 20:14:22 +0900

    feat. MySQL 연동

--- src/main/java/com/devandy/studypedia/StudyPediaApplication.java
+++ src/main/java/com/devandy/studypedia/StudyPediaApplication.java
@@ -1,24 +1,12 @@
 package com.devandy.studypedia;
 
-import com.devandy.studypedia.user.Role;
-import com.devandy.studypedia.utils.InitData;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class StudyPediaApplication implements CommandLineRunner {
-
-    @Autowired
-    private InitData initData;
-
-    @Override
-    public void run(String... args) throws Exception {
-        initData.injection();
-    }
+public class StudyPediaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudyPediaApplication.class, args);
